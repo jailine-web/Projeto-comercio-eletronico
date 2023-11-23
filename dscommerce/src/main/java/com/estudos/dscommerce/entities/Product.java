2fb4751b05649b423edc978b4699f1b682ea1767
Jailine <jay.santana964@gmail.com>
2023-11-23 19:34:55 -0300

    Inserindo o relacionamento muitos para muitos nas classes product e category

--- dscommerce/src/main/java/com/estudos/dscommerce/entities/Product.java
+++ dscommerce/src/main/java/com/estudos/dscommerce/entities/Product.java
@@ -9,6 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,6 +31,10 @@ public class Product {
 	
 	private String imgUrl;
 	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
