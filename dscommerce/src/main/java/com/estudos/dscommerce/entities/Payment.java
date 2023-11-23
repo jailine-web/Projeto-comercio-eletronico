867166b640232dba9d7e779f5ccc1e029d19d4b3
Jailine <jay.santana964@gmail.com>
2023-11-23 17:14:25 -0300

    Criação/mapeamento da classe Payment

---
+++ dscommerce/src/main/java/com/estudos/dscommerce/entities/Payment.java
@@ -0,0 +1,90 @@
+package com.estudos.dscommerce.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+import com.estudos.dscommerce.utils.OrderStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_payments")
+public class Payment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+	
+	private OrderStatus status;
+	private Order order;
+
+	public Payment() {
+
+	}
+
+	public Payment(Long id, Instant moment, OrderStatus status, Order order) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
