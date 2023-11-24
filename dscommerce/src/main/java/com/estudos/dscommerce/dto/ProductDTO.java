949092647b2cd53b1582cfc78ced36404f53397e
Jailine <jay.santana964@gmail.com>
2023-11-24 14:55:30 -0300

    Criando da camada service e dos DTOs

---
+++ dscommerce/src/main/java/com/estudos/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,54 @@
+package com.estudos.dscommerce.dto;
+
+import com.estudos.dscommerce.entities.Product;
+
+public class ProductDTO {
+
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	public ProductDTO() {
+		
+	}
+
+	public ProductDTO(Long id, String name, String description, 
+			          Double price, String imgUrl) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+
+	public ProductDTO(Product product) {
+		id = product.getId();
+		name = product.getName();
+		description = product.getDescription();
+		price = product.getPrice();
+		imgUrl = product.getImgUrl();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	
+}
---
+++ dscommerce/src/main/java/com/estudos/dscommerce/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.estudos.dscommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.estudos.dscommerce.dto.ProductDTO;
+import com.estudos.dscommerce.entities.Product;
+import com.estudos.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	private ProductRepository productRepository;
+	
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findbyId(Long id) {
+		Optional<Product> result = productRepository.findById(id);
+		Product product = result.get();
+		ProductDTO dto = new ProductDTO(product);
+		
+		return dto;
+	}
+}
