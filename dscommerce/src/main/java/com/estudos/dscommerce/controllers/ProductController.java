8c5522cf0e8a4c36693e39ca5f56783ab25a7e02
Jailine <jay.santana964@gmail.com>
2023-11-24 15:11:53 -0300

    Criando a camada controller e o método buscar produto por id

---
+++ dscommerce/src/main/java/com/estudos/dscommerce/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.estudos.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudos.dscommerce.dto.ProductDTO;
+import com.estudos.dscommerce.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+	private ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ProductDTO findbyId(@PathVariable Long id) {
+		 
+		ProductDTO dto = productService.findbyId(id);
+		return dto;
+	}
+	
+	
+}
