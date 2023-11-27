c6e3c12e6a8a8bf691040d8ab0db23005e9ee2da
Jailine <jay.santana964@gmail.com>
2023-11-27 16:23:32 -0300

    Tratando exceções com resposta customizada (no controller)- anotação @ControllerAdvice

---
+++ dscommerce/src/main/java/com/estudos/dscommerce/controllers/handlers/ControllerExceptionHandle.java
@@ -0,0 +1,25 @@
+package com.estudos.dscommerce.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.estudos.dscommerce.dto.CustomError;
+import com.estudos.dscommerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandle {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+	HttpStatus status = HttpStatus.NOT_FOUND;
+	CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+	return ResponseEntity.status(status).body(err);
+	}
+	
+}
